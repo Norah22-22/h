b65ad84f9c084d973e840f6cec5460f6ccb6c976
نورة <83267402+Norah22-22@users.noreply.github.com>
2024-12-19 12:54:04 +0300

    hi

---
+++ por2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using System.Linq.Expressions;
+
+public class Program
+{
+public static void Main(){
+  // string Name ="Norah";
+  // Console.WriteLine(Name);
+  int[] Array = new int [5];
+ Console.WriteLine(Array.Length);
+
+}
+} 
+
+
+
+
+
+// public class p
+// {
+// {
+// Array = new Array[1,2,3,4,5];
+//  Console.WriteLine(Array);}}
+
+ 
+/*
+  int a=4;
+int b=2;
+return(a,b);
+} 
+
+public class static ();{
+
+  int Valou =  Max.Valou
+
+}
+
+*/
+
